453e30c3c65aecf37526ae266e0aef019fb58fe3
splicedbread <jake.vr.84116@gmail.com>
2019-05-07 19:25:16 -0700

    Added FreeCellResources header file
    This file is a self containment of the resources provided int the
    Resources folder

---
+++ FreeCellResources.h
@@ -0,0 +1,29 @@
+#ifndef FREECELLRESOURCES_H
+#define FREECELLRESOURCES_H
+
+/*///////////////////////////////////////////////////
+	Author: Jacob Vanderkarr
+	Purpose: A centralized resource management file
+*////////////////////////////////////////////////////
+
+#define RES_CARD "./Resources/CardBlank.png"
+#define RES_CLUB "./Resources/Club.png"
+#define RES_SPADE "./Resources/Spade.png"
+#define RES_HEART "./Resources/Heart.png"
+#define RES_DIAMOND "./Resources/Diamond.png"
+
+#define RES_ACE "./Resources/Ace.png"
+#define RES_TWO "./Resources/Two.png"
+#define RES_THREE "./Resources/Three.png"
+#define RES_FOUR "./Resources/Four.png"
+#define RES_FIVE "./Resources/Five.png"
+#define RES_SIX "./Resources/Six.png"
+#define RES_SEVEN "./Resources/Seven.png"
+#define RES_EIGHT "./Resources/Eight.png"
+#define RES_NINE "./Resources/Nine.png"
+#define RES_TEN "./Resources/Ten.png"
+#define RES_JACK "./Resources/Jack.png"
+#define RES_QUEEN "./Resources/Queen.png"
+#define RES_KING "./Resources/King.png"
+
+#endif // !FREECELLRESOURCES_H
